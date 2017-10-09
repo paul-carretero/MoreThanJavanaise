8a0a6678e9f8ba3aead9f431b54f226d26868d0f
Paul Carretero <paul.carretero@gmail.com>
2017-10-09 08:57:04 +0200

    init tests Junit

---
+++ Javanaise/src/tests/genericTest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class testCoord {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
